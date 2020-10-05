a4d29712932318d2bacf4edfe0c7352e7ddfcdfd
pradipbabar <65601230+pradipbabar@users.noreply.github.com>
2020-10-05 07:56:43 +0530

    Update patten printing.py

--- patten printing.py
+++ patten printing.py
@@ -1,30 +1,8 @@
-# Exercise 4
-# Pattern Printing
-# Input = Integer n
-# Boolean = True or False
-#
-# True n=5
-# *
-# **
-# ***
-# ****
-#
-# False n=5
-# ****
-# ***
-# **
-# *
-# print("Pattern printing")
-# num = int(input("Enter num how many rows you want : "))
-# print("Enter 1 or 0")
-# bool_val = input("1 for True value or 0 for False : ")
-# if bool_val=="1":
-#     for i in range(0,num+1):
-#         print("*"*i)
-#
-# if bool_val=="0":
-#     for i in range(num,0,-1):
-#         print("*"* i)
+# have a nice day 
+
+    
+    for i in range(num,0,-1):
+
 
 
 a = int(input("please add number of line you want to print"))
